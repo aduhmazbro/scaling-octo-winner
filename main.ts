14ad55b814c73403f533c7e99bd2ddec04ffff96
Mazbro <140772578+aduhmazbro@users.noreply.github.com>
2024-01-19 20:02:12 +0700

    test: set response object based on deployment type

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-Deno.serve((_) => new Response("Hello world!"));
+import { load } from "https://deno.land/std/dotenv/mod.ts";
+
+async function handler(request: Request): Promise<Response> {
+    await load({
+        envPath: `.env_${request.url.includes(Deno.env.get("DENO_DEPLOYMENT_ID")!) ? 'preview' : 'prod'}`,
+        export: true
+    });
+
+    return new Response(Deno.env.get("GREETINGS")!);
+}
+
+Deno.serve(handler);
